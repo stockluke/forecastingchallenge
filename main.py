504ecad9cc9bb5d9332bc167ed591834336a491c
Luke <stockluke@live.com>
2021-02-07 13:11:34 -0800

    Adding login and registration functionality..

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '1cf72f84bad4015fe935d6344b81234b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 posts = [
     {
-        'key1':'data1',
-        'key2':'data2',
-        'key3':'data3',
-        'key4':'data4'
+        'key1': 'data1',
+        'key2': 'data2',
+        'key3': 'data3',
+        'key4': 'data4'
     },
     {
         'key1': 'data5',
@@ -15,6 +21,7 @@ posts = [
         'key4': 'data8'
     }
 ]
+
 
 @app.route('/')
 @app.route('/home')
@@ -25,6 +32,27 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@admin.com' and form.password.data == 'password':
+            flash('You have been logged in!','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
